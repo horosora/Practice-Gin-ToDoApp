9368941d75f600d969aa29dbb4299d3c95824e3e
horosora <horosora@gmail.com>
2019-02-02 21:38:02 +0900

    add ToDo struct

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
 )
+
+type ToDo struct {
+	gorm.Model
+	Contents string
+}
 
 func main() {
 	router := gin.Default()
